86762bf828d4f0a88c22dfb817efd58e972dcab7
mandelmonkey <chrismoss411@gmail.com>
2019-11-16 10:00:29 +0000

    na

--- helpers/caps.js
+++ helpers/caps.js
@@ -2,7 +2,7 @@ exports.ios = {
   browserName: '',
   //'appium-version': '1.6',
   platformName: 'iOS',
-  platformVersion: '13.0',//13.0
+  platformVersion: '13.2',//13.0
   deviceName: 'iPhone X',//'iPhone 11 Pro Max',
   automationName: "XCUITest",
       useNewWDA:true,

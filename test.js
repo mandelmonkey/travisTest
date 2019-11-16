719c9968318c88827639c7ede62c9e2920b6fd34
mandelmonkey <chrismoss411@gmail.com>
2019-11-16 16:05:24 +0000

    na

--- test.js
+++ test.js
@@ -1,21 +1,22 @@
 "use strict";
 
 var wd = require("wd");
-
+console.log("h1")
 describe("ios simple", function () {
-  this.timeout(10000000);
   var driver;
   var allPassed = true;
-
+  console.log("h2")
   before(function () {
+    console.log("h3")
     var serverConfig = {
-      host: 'localhost',
+      host: '0.0.0.0',
       port: 4723
     };
-
+    console.log("h4")
     driver = wd.promiseChainRemote(serverConfig);
+    console.log("h5")
     require("./helpers/logging").configure(driver);
-
+    console.log("h6")
     var desired = {
       browserName: '',
       platformName: 'iOS',
@@ -27,11 +28,12 @@ describe("ios simple", function () {
       showXcodeLog: true,
       app: process.cwd() + "/build/iphone/build/Products/Debug-iphonesimulator/travisTest.app"
     };
-
+    console.log("h7")
     return driver.init(desired);
   });
 
   after(function () {
+    console.log("h8")
     return driver
       .quit()
       .finally(function () {
@@ -42,10 +44,12 @@ describe("ios simple", function () {
   });
 
   afterEach(function () {
+    console.log("h9")
     allPassed = allPassed && this.currentTest.state === 'passed';
   });
 
   it("Should do a test", function () {
+    console.log("h10")
     return true;
   });
 });
